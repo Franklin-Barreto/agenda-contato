841a794f3464ec6df2bac041ca8239562721ce30
Franklin Barreto <franklin-barreto@hotmail.com>
2021-09-14 08:09:20 -0300

    initial data and database settings

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO user (name,email,password) VALUES ('Franklin','franklin@hotmail.com','$2a$12$pdlJY7IGuULz44gDcwzOKeW0YhIGRwPG3aHUw5TxMxgUV60KEwp6y');
+
+INSERT INTO role VALUES(null,'ROLE_USUARIO');
+INSERT INTO role VALUES(null,'ROLE_ADM');
+
+INSERT INTO user_roles VALUES (1,1);
