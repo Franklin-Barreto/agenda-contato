b176ecc4c205c5d85f789215281fce5a619941d1
Franklin Barreto <franklin-barreto@hotmail.com>
2021-09-22 23:53:48 -0300

    jwt security

---
+++ src/main/java/br/com/santander/agenda/config/security/LoginDto.java
@@ -0,0 +1,20 @@
+package br.com.santander.agenda.config.security;
+
+public class LoginDto {
+
+	private String email;
+	private String password;
+
+	public LoginDto(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
